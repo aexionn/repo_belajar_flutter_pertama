405e4106cc364b89abe748f70538c09a433c4180
unknown <corpsesbag@gmail.com>
2026-04-30 23:03:30 +0800

    First commit

---
+++ lib/screens/consultation_list_screen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:belajar_flutter_hit_api/models/consultation.dart';
+import 'package:belajar_flutter_hit_api/screens/consultation_form_screen.dart';
+import 'package:belajar_flutter_hit_api/services/api_service.dart';
+import 'package:intl/intl.dart';
+
+class ConsultationListScreen extends StatefulWidget {
+  const ConsultationListScreen({super.key});
+
+  @override
+  State<ConsultationListScreen> createState() => _ConsultationListScreenState();
+}
+
+class _ConsultationListScreenState extends State<ConsultationListScreen> {
+  final ApiService apiService = ApiService();
+  late Future<List<Consultation>> consultaion;
+
+  void refreshData() {
+    setState(() {
+      consultaion = apiService.getConsultation();
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    consultaion = apiService.getConsultation();
+  }
+
+  void deleteData(int id) async {
+    Navigator.pop(context);
+    await apiService.deleteData(id);
+
+    refreshData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Antrian Klinik')),
+      body: FutureBuilder<List<Consultation>>(
+        future: consultaion,
+        builder: (builder, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text("Error ${snapshot.error}"));
+          }
+
+          final data = snapshot.data ?? [];
+
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              final item = data[index];
+
+              return Card(
+                child: ListTile(
+                  isThreeLine: true,
+                  contentPadding: EdgeInsets.all(10),
+                  leading: Text(DateFormat("dd MMM").format(item.date)),
+                  trailing: Text(
+                    "${item.queueNumber}",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  title: Text(item.name),
+                  subtitle: Text("${item.poli} - ${item.complaint}"),
+                  onTap: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => ConsultationFormScreen(
+                          id: item.id,
+                          complaint: item.complaint,
+                          date: DateFormat("yyyy-mm-dd").format(item.date),
+                          name: item.name,
+                          poli: item.poli,
+                        ),
+                      ),
+                    );
+                    if (result == true) refreshData();
+                  },
+                  onLongPress: () => showDialog(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      title: Text("Apakah kamu mau menghapus data ini?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text("Batal"),
+                        ),
+                        TextButton(
+                          onPressed: () => deleteData(item.id),
+                          child: Text("Hapus"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => ConsultationFormScreen()),
+          );
+
+          if (result == true) {
+            refreshData();
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
